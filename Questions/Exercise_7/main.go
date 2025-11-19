273cafa9dcccb573c06a59102d1e33d5cd851fb3
Ankit Mishra <ankit1703@gmail.com>
2025-11-19 12:10:25 +0530

    Buffered channels

--- Questions/Exercise_7/main.go
+++ Questions/Exercise_7/main.go
@@ -2,10 +2,23 @@
 
 package main
 
+import "fmt"
+
 func printNumbers(n int, ch chan int) {
 
+	for i := 1; i <= n; i++ {
+		ch <- i
+	}
+
+	close(ch)
 }
 
 func main() {
 
+	ch := make(chan int, 5)
+	go printNumbers(5, ch)
+
+	for x := range ch {
+		fmt.Println(x)
+	}
 }
