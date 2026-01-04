6d2119246a1719f941593d56c2fcab0ce287be5c
Ankit Mishra <ankit1703@gmail.com>
2026-01-04 14:23:01 +0530

    Code updated.

--- Interface/EmptyInterface/main.go
+++ Interface/EmptyInterface/main.go
@@ -1,3 +1,5 @@
+// All type has zero methods so they satisfy empty interface.
+
 package main
 
 import "fmt"
@@ -8,4 +10,10 @@ func PrintAnything(v interface{}) {
 
 func main() {
 	PrintAnything("Hello World !!")
+	PrintAnything(2)
+	PrintAnything(true)
+
+	var data []interface{}
+	data = append(data, 1, "hello", true)
+
 }
